4202ccf4270e19825ba868ce8aa5dc94b38f1bed
anmb <anmb@itu.dk>
2021-09-05 12:10:55 +0200

    Tested and implemented IsLeapYear()

--- assignment00.Tests/UnitTest1.cs
+++ assignment00.Tests/UnitTest1.cs
@@ -6,14 +6,40 @@ namespace assignment00.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void main_when_something_returns_something()
+        [Theory]
+        [InlineData(4)]
+        [InlineData(40)]
+        [InlineData(396)]
+        [InlineData(2016)]
+        public void IsLeapYear_when_given_number_divisible_by_4_returns_true(int year)
         {
-            var writer = new StringWriter();
+            bool result = Program.IsLeapYear(year);
+            Assert.Equal(true, result);
+        }
 
-            Console.SetOut(writer);
-            
+        [Theory]
+        [InlineData(200)]
+        [InlineData(1800)]
+        [InlineData(2100)]
+        [InlineData(2900)]
+        public void IsLeapYear_when_given_number_divisible_by_4_and_100_returns_false(int year)
+        {
+            bool result = Program.IsLeapYear(year);
+          
+            Assert.Equal(false, result);
+        }
 
+
+        [Theory]
+        [InlineData(400)]
+        [InlineData(4000)]
+        [InlineData(1600)]
+        [InlineData(2000)]
+        public void IsLeapYear_when_given_number_divisible_by_400_returns_true(int year)
+        {
+            bool result = Program.IsLeapYear(year);
+          
+            Assert.Equal(true, result);
         }
     }
 }
--- assignment00/Program.cs
+++ assignment00/Program.cs
@@ -2,11 +2,15 @@
 
 namespace assignment00
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
+         
+        }
+
+        public static bool IsLeapYear(int year) {
+           return year%4==0 && year%100!=0 ? true : year%400==0;
         }
     }
 }
