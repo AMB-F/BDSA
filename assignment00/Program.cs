1cf4cf7ca27cd5c12c5b7b0b96a1e98f02485381
AMB-F <90040261+AMB-F@users.noreply.github.com>
2021-09-09 17:10:23 +0200

    Leapyear function (#2)
    * Tested and implemented IsLeapYear()
    
    * Added UI for program
    
    * Tested and added error handling
    
    * Added documentation

--- assignment00/Program.cs
+++ assignment00/Program.cs
@@ -2,11 +2,46 @@
 
 namespace assignment00
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static Boolean validYearEntered = false;
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
+            Console.WriteLine("Please enter a year and hit [enter]");
+            while (!validYearEntered)
+            {
+                try
+                {
+                    IsLeapYearUI();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Please enter a valid year after 1582");
+                }
+            }
+        }
+
+        public static void IsLeapYearUI()
+        {
+            string input = Console.ReadLine();
+            int yearInput;
+
+            bool isInteger = int.TryParse(input, out yearInput);
+            if (!isInteger)
+            {
+                throw new FormatException();
+            }
+            else if (yearInput < 1582)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            Console.WriteLine(IsLeapYear(yearInput) ? "yay" : "nay");
+            validYearEntered = true;
+        }
+
+        public static bool IsLeapYear(int year)
+        {
+            return year % 4 == 0 && year % 100 != 0 ? true : year % 400 == 0;
         }
     }
 }
