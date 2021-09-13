66b6d243eb509a4e1c5996ebc55ebb9dc15274c4
anmb <anmb@itu.dk>
2021-09-13 17:09:55 +0200

    Tested and added methods flatten, filter, SplitLine, Resolution

--- assignment01/Assignment1/Iterators.cs
+++ assignment01/Assignment1/Iterators.cs
@@ -7,12 +7,28 @@ namespace Assignment1
     {
         public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items)
         {
-            throw new NotImplementedException();
+            foreach (IEnumerable<T> streams in items)
+            {
+                foreach (T item in streams)
+                    yield return item;
+            }
         }
 
         public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate)
         {
-            throw new NotImplementedException();
+            foreach (T item in items)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+
+            }
+        }
+        public static bool Even(int i)
+        {
+            return i % 2 == 0;
         }
     }
+
 }
--- assignment01/Assignment1/RegExpr.cs
+++ assignment01/Assignment1/RegExpr.cs
@@ -1,23 +1,40 @@
 using System;
 using System.Collections.Generic;
-
+using System.Text.RegularExpressions;
 namespace Assignment1
 {
     public static class RegExpr
     {
         public static IEnumerable<string> SplitLine(IEnumerable<string> lines)
         {
-            throw new NotImplementedException();
+            foreach (var sentence in lines)
+            {
+                foreach (var word in Regex.Split(sentence, "\\s+"))
+                {
+                    yield return word;
+                }
+            }
         }
 
-        public static IEnumerable<(int width, int height)> Resolution(string resolutions)
+        public static IEnumerable<(int width, int height)> Resolution(IEnumerable<string> resolutions)
         {
-            throw new NotImplementedException();
+            var regex = new Regex(@"(?<width>[\d]*)[x](?<height>[\d]*)");
+            foreach (var line in resolutions)
+            {
+                foreach (Match match in regex.Matches(line))
+                {
+                    yield return (Int32.Parse(match.Groups["width"].Value), Int32.Parse(match.Groups["height"].Value));
+                }
+            }
         }
 
         public static IEnumerable<string> InnerText(string html, string tag)
         {
-            throw new NotImplementedException();
+            var regex = new Regex(@"<("+tag+")>(?<text>.*)</(\\1)>");
+            foreach(Match match in regex.Matches(html)){
+                yield return match.Groups["text"].Value;
+            }
+
         }
     }
 }
